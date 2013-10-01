0989a7d4ac829c3c676349f05c89940785655bc7
Doug Henderson <dhenderson@maestrodev.com>
2013-10-01 11:37:23 -0700

    MAESTRO-3228 Add ability to handle HostKeyMismatch

--- src/ssh_common.rb
+++ src/ssh_common.rb
@@ -6,33 +6,34 @@ require 'net/ssh'
 
 module MaestroDev
   module Plugin
-  
+
     class SSHCommon < Maestro::MaestroWorker
       DEFAULT_KEY_TYPE = 'ssh-rsa'
       DEFAULT_RETRIES = 10
       DEFAULT_WAIT = 10
       DEFAULT_PORT = 22
-  
+
       private
-  
+
       def validate_parameters
         @host = get_field('host', '')
-        @port = get_field('port', 0)
+        @port = get_int_field('port', DEFAULT_PORT)
         @user = get_field('user', '')
         @key_path = get_field('key_path', '')
         @key_type = get_field('key_type', DEFAULT_KEY_TYPE)
         @password = get_field('password')
-        @retries = get_field('retries', DEFAULT_RETRIES)
-        @wait = get_field('wait', DEFAULT_WAIT)
-  
+        @retries = get_int_field('retries', DEFAULT_RETRIES)
+        @wait = get_int_field('wait', DEFAULT_WAIT)
+        @update_host_key = get_boolean_field('update_host_key')
+
         errors = []
         errors << 'Invalid host' if @host.empty?
         errors << 'Invalid user' if @user.empty?
         errors << 'Invalid key, not found' if !@key_path.empty? and !File.exists?(@key_path)
-  
+
         errors
       end
-  
+
       # Call me with a block, and I'll connect to the server, run your block, and close the connection before leaving
       # Will call 'validate_parameters' which should be overridden if req'd as such
       # def validate_parameters
@@ -42,16 +43,16 @@ module MaestroDev
       # end
       def do_ssh
         validate_parameters
-  
+
         auth_types = []
         auth_types << 'PASSWORD' if @password && !@password.empty?
         auth_types << "KEY (#{@key_type})"
-  
+
         connection = @host
         connection += " port #{@port}" if @port != 0
-  
+
         write_output("\nConnecting to #{connection} as #{@user} using auth [#{auth_types.join(', ')}]")
-  
+
         session = start(@host,
           @user,
           @key_path, 
@@ -69,10 +70,10 @@ module MaestroDev
       ensure
         close
       end
-  
+
       def start(server, user, key_path, key_type, password, port, retries, wait)
         trys = 0
-  
+
         while trys <= retries 
           trys += 1
 
@@ -83,13 +84,30 @@ module MaestroDev
                       :compression => 'zlib',
                       :port => port,
                       :auth_methods => ["publickey", "hostbased", "password"]}
-  
+
             if !key_path.empty?
               params[:keys] = [ key_path ]
             end
-  
-            @session = Net::SSH.start(server, user, params)
-  
+
+            begin
+              @session = Net::SSH.start(server, user, params)
+            rescue Net::SSH::HostKeyMismatch => e
+              if @update_host_key
+                write_output("\nUpdating changed host-key")
+                e.remember_host!
+
+                # Easy retry... if it still fails, it'll drop thru to default exception handler
+                @session = Net::SSH.start(server, user, params)
+              else
+                # If update_host_key not set, let this error propagate
+                raise PluginError, "Host key mismatch - this happens when the host being connected to " +
+                  "is not the same as was connected to previously.  In some cases this is expected, " +
+                  "in other cases it can be cause for concern.\nIf acceptable, set the 'update_host_key' " +
+                  "property to true, and Maestro will attempt to update the local copy of the " +
+                  "host-key when/if it changes."
+              end
+            end
+
             write_output("\nConnected", :buffer => true)
             trys = retries + 1
           rescue Errno::ECONNREFUSED => e
@@ -102,7 +120,7 @@ module MaestroDev
         end
         @session
       end
-  
+
       def close()
         @session.andand.close if !@session.andand.closed?
       end
