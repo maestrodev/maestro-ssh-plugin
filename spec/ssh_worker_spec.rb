dad0fda9b8e01d5d1fd42eb90dafd454e328d119
Carlos Sanchez <csanchez@maestrodev.com>
2013-11-13 13:06:30 +0100

    Add a spec to test private key filename expansion

--- spec/ssh_worker_spec.rb
+++ spec/ssh_worker_spec.rb
@@ -26,10 +26,11 @@ describe MaestroDev::Plugin::SSHWorker do
 
   describe '/ssh/execute' do
 
-    it 'should detect if key-file is not found' do
-      workitem = {'fields' => fields.merge({'key_path' => '/not_real/'})}
-      subject.perform(:execute, workitem)
-      workitem['fields']['__error__'].should include('Invalid key, not found')
+    context "when key file does not exist" do
+      let(:key_path) { '~/not_real/' }
+      let(:fields) { super().merge({'key_path' => key_path}) }
+      before { subject.perform(:execute, {'fields' => fields}) }
+      its(:error) { should eq("Config Errors: Invalid key, not found: #{File.expand_path(key_path)}") }
     end
 
     it "should connect to a remote server and execute a given set of commands" do
@@ -65,22 +66,25 @@ describe MaestroDev::Plugin::SSHWorker do
       workitem['fields']['__error__'].should include('Error in SSH connection: kelly')
     end
 
-    it "should set the error field correctly if a command fails (ignore == false)" do
-      subject.stubs(:start)
-      subject.stubs(:perform_command).raises(MaestroDev::Plugin::SSHWorker::SSHCommandError, "ehh?")
-      workitem = {'fields' => fields}
-      subject.perform(:execute, workitem)
-      workitem['fields']['__error__'].should include('ehh?')
-      workitem['__output__'].should include("\nOf 2 commands: 1 excecuted, 1 failed. (ignore_errors = false)")
-    end
+    context "when a command fails" do
+      before do
+        subject.stubs(:start)
+        subject.stubs(:perform_command).raises(MaestroDev::Plugin::SSHWorker::SSHCommandError, "ehh?")
+      end
 
-    it "should not set the error field if a command fails (ignore == true)" do
-      subject.stubs(:start)
-      subject.stubs(:perform_command).raises(MaestroDev::Plugin::SSHWorker::SSHCommandError, "wha?")
-      workitem = {'fields' => fields.merge({'ignore_errors' => true})}
-      subject.perform(:execute, workitem)
-      workitem['fields']['__error__'].should be_nil
-      workitem['__output__'].should include("\nOf 2 commands: 2 excecuted, 2 failed. (ignore_errors = true)")
+      it "should set the error field correctly if a command fails (ignore == false)" do
+        workitem = {'fields' => fields}
+        subject.perform(:execute, workitem)
+        workitem['fields']['__error__'].should include('ehh?')
+        workitem['__output__'].should include("\nOf 2 commands: 1 excecuted, 1 failed. (ignore_errors = false)")
+      end
+
+      it "should not set the error field if a command fails (ignore == true)" do
+        workitem = {'fields' => fields.merge({'ignore_errors' => true})}
+        subject.perform(:execute, workitem)
+        workitem['fields']['__error__'].should be_nil
+        workitem['__output__'].should include("\nOf 2 commands: 2 excecuted, 2 failed. (ignore_errors = true)")
+      end
     end
   end
 end
